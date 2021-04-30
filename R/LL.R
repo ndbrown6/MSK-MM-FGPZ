045763b1168eee108c51e9a004da58d9b13e511c
David Brown <ndbrown6@gmail.com>
2021-04-30 14:42:59 -0400

    Simulated data

--- R/LL.R
+++ R/LL.R
@@ -1,7 +1,5 @@
-'LL' <- function(m_j, c_j, n, b) {
-	ll = 0
-	for (j in 1:n) {
-		ll = ll + dbinom(x = m_j[j], size = c_j[j], prob = .MMEnv$v_b[b], log = TRUE) + dbinom(x = c_j[j] - m_j[j], size = c_j[j], prob = 1 - .MMEnv$v_b[b], log = TRUE)
-	}
+'LL' <- function(m_j, c_j, b, alpha = .5) {
+	ll = alpha*dbinom(x = m_j, size = c_j, prob = .MMEnv$v_b[b]) * (1-alpha)*dbinom(x = c_j - m_j, size = c_j, prob = 1 - .MMEnv$v_b[b])
 	return(invisible(ll))
 }
+
--- ext/test.R
+++ ext/test.R
@@ -1,25 +1,36 @@
-b = 1:4
-v_b = c(.25, .125, .0625, .03125)
+#==================================================
+# David Brown
+# brownd7@mskcc.org
+#==================================================
+library("mskmmfgpz")
+data("vb=1")
 
-'LL' <- function(m_j, c_j, n, b) {
-	ll = 0
-	for (j in 1:n) {
-		ll = ll + dbinom(x = m_j[j], size = c_j[j], prob = v_b[b], log = TRUE) + dbinom(x = c_j[j] - m_j[j], size = c_j[j], prob = 1 - v_b[b], log = TRUE)
-	}
-	return(invisible(ll))
-}
-
-set.seed(0)
-n = 50
-c = round(runif(n = n, min = 100, max = 1000))
-m = abs(round((c*v_b[4]) + runif(n = n, min = -10, max = 10)))
-
-res = rep(NA, length = 4)
+lls = vector(mode = "numeric", length = 4)
 for (i in 1:4) {
-	res[i] = LL(m, c, n, i)
+	ll = vector(mode = "numeric", length = n)
+	for (j in 1:n) {
+		ll[i] = LL(m_j = m[j], c_j = c[j], b = i, alpha = .5)
+	}
+	lls[i] = sum(ll)
 }
 
 par(mar=c(6.1, 6.5, 4.1, 1.1))
-barplot(height = -res, space = 0, names.arg = 1:4, las = 1, xlab = "")
+barplot(height = lls, space = 0, names.arg = 1:4, las = 1, xlab = "")
+mtext(side=1, text=expression(v[b]), line=4, cex=1.5)
+mtext(side=2, text=expression(LL), line=4, cex=1.5)
+
+
+alpha = seq(from = .5, to = 1, length = 100)
+lls = vector(mode = "numeric", length = 4)
+for (i in 1:4) {
+	ll = vector(mode = "numeric", length = n)
+	for (j in 1:n) {
+		ll[i] = LL(m_j = m[j], c_j = c[j], b = i, alpha = .5)
+	}
+	ll_s[i] = sum(ll)
+}
+
+par(mar=c(6.1, 6.5, 4.1, 1.1))
+barplot(height = ll_s, space = 0, names.arg = 1:4, las = 1, xlab = "")
 mtext(side=1, text=expression(v[b]), line=4, cex=1.5)
 mtext(side=2, text=expression(LL), line=4, cex=1.5)
