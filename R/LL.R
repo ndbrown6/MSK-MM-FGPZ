8ca34f8e116e96941bf03d2cc274a14a9364052e
David Brown <ndbrown6@gmail.com>
2021-04-29 23:54:39 -0400

    R

--- R/LL.R
+++ R/LL.R
@@ -1,4 +1,4 @@
-'LL' <- function(m_j, c_j, n, v_b, alpha) {
+'LL' <- function(m_j, c_j, n, v_b) {
 	ll = 0
 	for (j in 1:n) {
 		ll = ll + dbinom(x = m_j[j], size = c_j[j], prob = v_b[b], log = TRUE) + dbinom(x = c_j[j] - m_j[j], size = c_j[j], prob = 1 - v_b[b], log = TRUE)
