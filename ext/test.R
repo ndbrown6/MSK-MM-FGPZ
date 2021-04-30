1528e44662907f0c6ab3174b7e91e8b389daa97d
David Brown <ndbrown6@gmail.com>
2021-04-30 17:13:18 -0400

    Various

--- ext/test.R
+++ ext/test.R
@@ -5,32 +5,20 @@
 library("mskmmfgpz")
 data("vb=1")
 
-lls = vector(mode = "numeric", length = 4)
-for (i in 1:4) {
+m = m + 100
+m[m>c] = c[m>c]
+
+alpha = seq(from = 1, to = 2, length = 100)
+LP_A = vector(mode = "numeric", length = 100)
+for (a in 1:length(alpha)) {
 	ll = vector(mode = "numeric", length = n)
-	for (j in 1:n) {
-		ll[i] = LL(m_j = m[j], c_j = c[j], b = i, alpha = .5)
+	for (i in 1:n) {
+		ll[i] = LL(m_j = m[i], c_j = c[i], b = 1, alpha = alpha[a])
 	}
-	lls[i] = sum(ll)
+	LP_A[a] = sum(log(ll))
 }
 
 par(mar=c(6.1, 6.5, 4.1, 1.1))
-barplot(height = lls, space = 0, names.arg = 1:4, las = 1, xlab = "")
-mtext(side=1, text=expression(v[b]), line=4, cex=1.5)
-mtext(side=2, text=expression(LL), line=4, cex=1.5)
-
-
-alpha = seq(from = .5, to = 1, length = 100)
-lls = vector(mode = "numeric", length = 4)
-for (i in 1:4) {
-	ll = vector(mode = "numeric", length = n)
-	for (j in 1:n) {
-		ll[i] = LL(m_j = m[j], c_j = c[j], b = i, alpha = .5)
-	}
-	ll_s[i] = sum(ll)
-}
-
-par(mar=c(6.1, 6.5, 4.1, 1.1))
-barplot(height = ll_s, space = 0, names.arg = 1:4, las = 1, xlab = "")
-mtext(side=1, text=expression(v[b]), line=4, cex=1.5)
+plot(alpha, LP_A, las = 1, xlab = "", ylab = "")
+mtext(side=1, text=expression(alpha), line=4, cex=1.5)
 mtext(side=2, text=expression(LL), line=4, cex=1.5)
