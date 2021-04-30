df7f684f6a4e0d2534b996c44f4fb251d26671f4
David Brown <ndbrown6@gmail.com>
2021-04-30 00:17:55 -0400

    LL

--- R/LL.R
+++ R/LL.R
@@ -1,7 +1,7 @@
-'LL' <- function(m_j, c_j, n, v_b) {
+'LL' <- function(m_j, c_j, n, b) {
 	ll = 0
 	for (j in 1:n) {
-		ll = ll + dbinom(x = m_j[j], size = c_j[j], prob = v_b[b], log = TRUE) + dbinom(x = c_j[j] - m_j[j], size = c_j[j], prob = 1 - v_b[b], log = TRUE)
+		ll = ll + dbinom(x = m_j[j], size = c_j[j], prob = .MMEnv$v_b[b], log = TRUE) + dbinom(x = c_j[j] - m_j[j], size = c_j[j], prob = 1 - .MMEnv$v_b[b], log = TRUE)
 	}
 	return(invisible(ll))
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .MMEnv <- new.env()
 
-b = 1:4
 v_b = c(.25, .125, .0625, .03125)
-assign("b", b, envir=.MMEnv)
 assign("v_b", v_b, envir=.MMEnv)
+
+rm(v_b)
