9c404200e21b9069d03e3c974e881ca3064c6a50
David Brown <ndbrown6@users.noreply.github.com>
2021-04-29 23:42:46 -0400

    Update LL.R

--- LL.R
+++ LL.R
@@ -1,5 +1,7 @@
-'LL' <- function()
-{
-
-
+'LL' <- function(m_j, c_j, n, v_b, alpha) {
+	ll = 0
+	for (j in 1:n) {
+		ll = ll + dbinom(x = m_j[j], size = c_j[j], prob = v_b[b], log = TRUE) + dbinom(x = c_j[j] - m_j[j], size = c_j[j], prob = 1 - v_b[b], log = TRUE)
+	}
+	return(invisible(ll))
 }
