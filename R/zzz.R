2eec54470251f015a75b604b3a139125e1a8b547
David Brown <ndbrown6@gmail.com>
2021-04-30 00:04:35 -0400

    Package Env

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,6 @@
-.GapEnv <- new.env()
-vN = c("c_ind", "c_is", "c_if", "c_chr", "c_len",
-	   "c_lrrBP", "c_bafBP", "c_lrr", "c_baf", "c_bafH",
-	   "c_nC", "c_nA", "c_nS", "c_CN", "c_BA",
-	   "c_conf", "c_sb", "c_CN1", "c_BA1", "c_rnk1")
-for (i in 1:length(vN)) {
-	assign(vN[i], i, envir=.GapEnv)
-}
+.MMEnv <- new.env()
+
+b = 1:4
+v_b = c(.25, .125, .0625, .03125)
+assign("b", b, envir=.MMEnv)
+assign("v_b", v_b, envir=.MMEnv)
