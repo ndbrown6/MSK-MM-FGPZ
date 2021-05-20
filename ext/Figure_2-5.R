e830b9b3486fd1085df518ae6f1ca204396ae442
David Brown <ndbrown6@gmail.com>
2021-05-19 23:19:14 -0400

    nb = 4 clusters

--- ext/Figure_2-5.R
+++ ext/Figure_2-5.R
@@ -32,16 +32,48 @@ data("vb=n")
 m = data %>% .[["N_Alt"]]
 c = data %>% .[["N_Total"]]
 
-# plot posterior densities of mixture model with actual data vb = n and nb = 5 cell generations
-LL = list()
-for (i in 1:10) {
+# bootstrap mixture model with actual data vb = n and nb = 1|2|3|4|5|6|7 cell generations
+LL = matrix(NA, nrow = 100, ncol = 7)
+LL = foreach(i=1:100) %dopar% {
 	print(i)
-	LL[[i]] = MixtureBB(m = m, c = c, nb = i)
+	index = sample(x = 1:length(m), size = rpois(n = 1,lambda = length(m)), replace = TRUE)
+	LL = vector(mode = "numeric", length = 7)
+	for (j in 1:7) {
+		LL[j] = MixtureBB(m = m[index], c = c[index], nb = j)$LL
+	}
+	return(invisible(LL))
 }
+LL = do.call(rbind, LL)
 
-.MMEnv$vb = LL[[5]]$vb
+data_ = dplyr::as_tibble(LL) %>%
+	reshape2::melt() %>%
+	dplyr::rename(vb = variable,
+		      LL = value) %>%
+	dplyr::as_tibble() %>%
+	dplyr::mutate(vb = gsub("V", "", vb, fixed=TRUE)) %>%
+	readr::type_convert() %>%
+	dplyr::mutate(n = rep(1:100, times = 7))
+
+plot_ = data_ %>%
+	ggplot(aes(x = vb, y = LL/1E3, group = n)) + 
+	geom_line(stat = "identity", alpha = .35, color = "#333333", size = .25) +
+	theme_classic() +
+	xlab(bquote(atop(" ", nu[b]))) +
+	ylab(expression("Log-Likelihood ("%.%10^-3~")")) +
+	scale_x_continuous(breaks = 1:7,
+			   labels = 1:7) +
+	scale_y_continuous(breaks = c(0, -1, -2, -3),
+			   labels = c("0", "       -1", "-2", "-3"))
+
+pdf(file = "_vb_LL.pdf", height = 4, width = 4)
+print(plot_)
+dev.off()
+
+# plot posterior densities of mixture model with actual data vb = n and nb = 4 cell generations
+LL = MixtureBB(m = m, c = c, nb = 4)
+.MMEnv$vb = LL$vb
 .MMEnv$max_iter = 1000
-LL0 = SymmBB(m = m, c = c, nb = 5)
+LL0 = SymmBB(m = m, c = c, nb = 4)
 
 data_ = do.call(rbind, LL0$p_bjr) %>%
 	dplyr::as_tibble() %>%
@@ -91,7 +123,7 @@ dev.off()
 
 plot_ = data_ %>%
 	ggplot(aes(x = V4, y = UUID, group = UUID)) + 
-	geom_density_ridges(stat = "density_ridges", fill = hex_cols[4], color = hex_cols[4], alpha = .75, bandwidth = .05) +
+	geom_density_ridges(stat = "density_ridges", fill = hex_cols[5], color = hex_cols[5], alpha = .75, bandwidth = .05) +
 	theme_classic() +
 	xlab(bquote(atop(" ", Pr(nu[b] ==4)))) +
 	ylab("") +
@@ -102,20 +134,7 @@ pdf(file = "p(vb=4).pdf", height = 10, width = 5)
 print(plot_)
 dev.off()
 
-plot_ = data_ %>%
-	ggplot(aes(x = V5, y = UUID, group = UUID)) + 
-	geom_density_ridges(stat = "density_ridges", fill = hex_cols[5], color = hex_cols[5], alpha = .75, bandwidth = .05) +
-	theme_classic() +
-	xlab(bquote(atop(" ", Pr(nu[b] ==5)))) +
-	ylab("") +
-	scale_x_continuous(limits = c(-0.1,1.1),
-			   breaks = c(0, .2, .4, .6, .8, 1))
-
-pdf(file = "p(vb=5).pdf", height = 10, width = 5)
-print(plot_)
-dev.off()
-
-# plot density of symmetric model with actual data vb = n and nb = 5 cell generations [updated]
+# plot density of symmetric model with actual data vb = n and nb = 4 cell generations [updated]
 .MMEnv$vb = 2^(-(2:6))
 n = 1E5
 ll = SymmLL(m = m, c = c, nb = 5)
@@ -124,9 +143,9 @@ sim_c = sample(x = c, size = n, replace = TRUE)
 sim_v = rbetabinom(n = n, size = sim_c, prob = sample(x = vb, size = n, prob = ll$r_b/sum(ll$r_b), replace = TRUE))/sim_c
 dens_x = density(sim_v, from=0, to=0.3, adjust = 1)
 
-.MMEnv$vb = LL[[5]]$vb
-ll = SymmBB(m = m, c = c, nb = 5)
-vb = .MMEnv$vb[1:5]
+.MMEnv$vb = LL[[4]]$vb
+ll = SymmBB(m = m, c = c, nb = 4)
+vb = .MMEnv$vb[1:4]
 sim_c = sample(x = c, size = n, replace = TRUE)
 sim_v = rbetabinom(n = n, size = sim_c, prob = sample(x = vb, size = n, prob = ll$r_b/sum(ll$r_b), replace = TRUE))/sim_c
 dens_y = density(sim_v, from=0, to=0.3, adjust = 1)
@@ -142,7 +161,7 @@ plot_ = dplyr::tibble(vaf = m/c) %>%
 		  color = "#AEA79F",
 		  alpha = .45,
 		  inherit.aes = FALSE) +
-	geom_vline(xintercept = .MMEnv$vb[1:5]*100, linetype = 1, size = .25) +
+	geom_vline(xintercept = .MMEnv$vb[1:4]*100, linetype = 1, size = .25) +
 	geom_vline(xintercept = (2^(-(2:6))*100), linetype = 3, size = .25) +
 	geom_line(data = dplyr::tibble(x = dens_x$x*100,
 				       y = dens_x$y),
@@ -189,11 +208,11 @@ plot_ = data_ %>%
 		Variant_Classification == "Splice_Site" ~ "Splice Site"
 	)) %>%
 	ggplot(aes(x = UUID, y = 100*VAF, ymin = CI95_Lower*100, ymax = CI95_Upper*100, fill = factor(vb), shape = Variant_Classification)) +
-	geom_hline(yintercept = .MMEnv$vb[1:5]*100, colour = "black", linetype = 1, size = .25) +
+	geom_hline(yintercept = .MMEnv$vb[1:4]*100, colour = "black", linetype = 1, size = .25) +
 	geom_hline(yintercept = (2^(-(2:6))*100), colour = "black", linetype = 3, size = .25) +
 	geom_pointrange(show.legend = FALSE) +
 	geom_point(stat = "identity", size = 2) +
-	scale_fill_manual(values = hex_cols) +
+	scale_fill_manual(values = hex_cols[c(1:3,5)]) +
 	scale_shape_manual(values = c("Frame Shift Deletion" = 21,
 				      "Frame Shift Insertion" = 22,
 				      "Missense Mutation" = 23,
@@ -205,8 +224,8 @@ plot_ = data_ %>%
 			   labels = seq(from = 0, to = 25, by = 5)) +
 	theme_classic() +
 	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1)) +
-	guides(fill = guide_legend(title = bquote(nu[b]), override.aes = list(shape = 21)),
-	       shape = guide_legend(title = "Variant Type"))
+	guides(fill = guide_legend(title = bquote(nu[b]), order = 1, override.aes = list(shape = 21)),
+	       shape = guide_legend(title = "Variant Type", order = 2))
 	
 pdf(file = "_VAF_by_Variant.pdf", width = 8, height = 6)
 print(plot_)
