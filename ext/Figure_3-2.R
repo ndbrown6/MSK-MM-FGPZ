236be45be5060f6c4d17722f7f5761ee20dea78f
David Brown <ndbrown6@gmail.com>
2021-05-19 22:09:38 -0400

    Update Figure_3-2.R

--- ext/Figure_3-2.R
+++ ext/Figure_3-2.R
@@ -27,22 +27,3 @@ hex_cols = c("#C1272D",
 'scientific_10' <- function(x) {
 	parse(text=gsub("+", "", gsub("e", " %.% 10^", scales::scientific_format()(x)), , fixed = TRUE))
 }
-
-data("vb=n")
-m = data %>% .[["N_Alt"]]
-c = data %>% .[["N_Total"]]
-
-# classify mosaic variants with nb = 5 cell generations
-LL = MixtureBB(m = m, c = c, nb = 5)
-
-.MMEnv$vb = LL$vb
-.MMEnv$max_iter = 1000
-LL0 = SymmBB(m = m, c = c, nb = 5)
-
-data_ = do.call(rbind, LL0$p_bjr) %>%
-	dplyr::as_tibble() %>%
-	dplyr::mutate(UUID = rep(paste(data$Case_ID, ":", data$Gene_Symbol, ":", data$HGVSp_Short), .MMEnv$n_run),
-		      VAF = rep(data$N_Alt/data$N_Total, .MMEnv$n_run)) %>%
-	dplyr::arrange(VAF) %>%
-	dplyr::mutate(UUID = factor(UUID, levels = unique(UUID), ordered = TRUE))
-
